b61822809caa99c3dc8cbd0be4a9ea8381797f62
amtoine <stevan.antoine@gmail.com>
2024-12-17 13:55:46 +0100

    add format keymap and "format on save"

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -19,5 +19,25 @@ return { {
     vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
     vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
     vim.keymap.set('n', 'grr', vim.lsp.buf.references)
+    vim.keymap.set('n', 'grf', vim.lsp.buf.format)
+
+    -- format on save
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client then
+          return
+        end
+
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = args.buf,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+            end
+          })
+        end
+      end
+    })
   end,
 } }
