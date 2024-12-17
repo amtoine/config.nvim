47b0a71c05ab63817166734d5f582a6d563163a2
amtoine <stevan.antoine@gmail.com>
2024-12-17 17:48:44 +0100

    make "find files" support Git with fallback

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -30,11 +30,25 @@ return { {
       pickers = {
         find_files = {
           theme = "ivy"
+        },
+        git_files = {
+          theme = "ivy"
         }
       },
     }
 
-    vim.keymap.set("n", "<leader>fd", builtin.find_files)
+    vim.keymap.set("n", "<leader>fd", function()
+      local opts = { prompt_title = "Find project files..." }
+
+      vim.fn.system("git rev-parse --is-inside-work-tree")
+
+      if vim.v.shell_error == 0 then
+        builtin.git_files(opts)
+      else
+        builtin.find_files(opts)
+      end
+    end
+    )
     vim.keymap.set("n", "<leader>fh", builtin.help_tags)
     vim.keymap.set("n", "<leader>fg", builtin.live_grep)
   end
