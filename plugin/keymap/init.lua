b51d29f604151ef5a3bbaa3c024d2c69bb663719
amtoine <stevan.antoine@gmail.com>
2024-12-17 10:16:25 +0100

    move "Lua execute" keymaps to "keymap/init.lua"

--- plugin/keymap/init.lua
+++ plugin/keymap/init.lua
@@ -4,10 +4,10 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("i", "<C-c>", "<Esc>", { silent = true })
 
 vim.keymap.set(
-    "n", "<leader>xs", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-    { desc = "replace all occurences of the work under the cursor" }
+    "n", "<leader>zr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    { desc = "replace all occurences of the word under the cursor" }
 )
-vim.keymap.set(
-    "n", "<leader>xx", "<cmd>!chmod +x %<CR>",
-    { silent = true, desc = "make the current buffer executable" }
-)
+vim.keymap.set("n", "<leader>zx", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>x", ":.lua<CR>", { silent = true })
+vim.keymap.set("v", "<leader>x", ":lua<CR>", { silent = true })
