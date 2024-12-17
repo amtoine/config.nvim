89302fd6a6eea1da454261077bba89ad77fcbd54
amtoine <stevan.antoine@gmail.com>
2024-12-17 11:28:37 +0100

    move LSP keymaps to `lua/custom/lsp.lua`

--- lua/custom/lsp.lua
+++ lua/custom/lsp.lua
@@ -1,5 +1,15 @@
 local nmap = require("custom._utils").nmap
 
+nmap('[d', vim.diagnostic.goto_prev)
+nmap(']d', vim.diagnostic.goto_next)
+
+nmap('<leader>le', vim.diagnostic.open_float)
+nmap('<leader>lq', vim.diagnostic.setloclist)
+
+nmap("<leader>lf", vim.lsp.buf.format)
+
+nmap('<leader>li', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end)
+
 local on_attach = function(_, bufnr)
   nmap('<leader>lr', vim.lsp.buf.rename, bufnr)
   nmap('<leader>lc', vim.lsp.buf.code_action, bufnr)
