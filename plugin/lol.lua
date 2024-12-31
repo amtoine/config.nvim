6ac4aaa01852c360a225f375193598bd19933b3b
amtoine <stevan.antoine@gmail.com>
2024-12-31 20:07:52 +0100

    add "URL menu entry"

--- plugin/lol.lua
+++ plugin/lol.lua
@@ -7,6 +7,7 @@ vim.cmd [[
   amenu PopUp.References         <cmd>lua vim.lsp.buf.references()<CR>
   amenu PopUp.-2-                <NOP>
   nmenu PopUp.Back               <c-t>
+  amenu PopUp.URL                gx
 ]]
 
 local popup_menu_group = vim.api.nvim_create_augroup("nvim_popupmenu", { clear = true })
@@ -21,9 +22,16 @@ vim.api.nvim_create_autocmd("MenuPopup", {
   callback = function()
     disable("PopUp.Definition")
     disable("PopUp.References")
+    disable("PopUp.URL")
+
     if vim.lsp.get_clients { bufnr = 0 }[1] then
       enable("PopUp.Definition")
       enable("PopUp.References")
+    end
+
+    local urls = require("vim.ui")._get_urls()
+    if vim.startswith(urls[1], "http") then
+      enable("PopUp.URL")
     end
   end
 })
